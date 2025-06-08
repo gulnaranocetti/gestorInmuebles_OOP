0c089e511f864bf138b1c43f8ef886c94d66cbf2
Gulnara <gulnara.nocetti@fing.edu.uy>
2025-06-07 21:36:39 -0300

    init ISuscriptor.h  Notificacion.h

---
+++ include/ISuscriptor.h
@@ -0,0 +1,17 @@
+#ifndef ISUSCRIPTOR_H
+#define ISUSCRIPTOR_H
+
+#include "Notificacion.h"
+#include "Inmobiliaria.h"
+#include <set>
+
+class ISuscriptor {
+public:
+    virtual void recibirNotificacion(const Notificacion& n) = 0;
+    virtual std::set<Notificacion> consultarNotificaciones() const = 0;
+    virtual void eliminarSuscripcion(Inmobiliaria* i) = 0;
+
+    virtual ~ISuscriptor() {}
+};
+
+#endif
---
+++ include/Notificacion.h
@@ -0,0 +1,21 @@
+#ifndef NOTIFICACION_H
+#define NOTIFICACION_H
+#include "DTFecha.h"
+#include "TipoPublicacion.h"
+#include "TipoInmueble.h"
+#include "DTUsuario.h"
+#include <string>
+
+class Notificacion {
+private:
+    DTFecha* fecha;
+    std::string textoPublicacion;
+    std::string inmobiliaria;
+    int codigoPublicacion;
+    TipoPublicacion tipoPublicacion;
+    TipoInmueble tipoInmueble;
+};
+
+#endif 
+
+
