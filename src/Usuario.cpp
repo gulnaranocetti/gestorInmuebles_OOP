2134edde544bd12cc192f26db03e427ea4ca34de
Anthony David Figueredo Pereira <anthony.figueredo@fing.edu.uy>
2025-06-11 18:44:19 -0300

    Add Usuario.cpp

---
+++ src/Usuario.cpp
@@ -0,0 +1,42 @@
+#include "Usuario.h"
+
+void Usuario::setNickname(std::string _nickname){
+        nickname = _nickname;
+    }
+
+    void Usuario::setNombre(std::string _nombre){
+        nombre = _nombre;
+    }
+
+    void Usuario::setContrasena(std::string _contrasena){
+        contrasena = _contrasena;
+    }
+
+    void Usuario::setEmail(std::string _email){
+        email = _email;
+    }
+
+    std::string Usuario::getNickname(){
+        return nickname;
+    }
+
+
+    std::string Usuario::getNombre(){
+        return nombre;
+    }
+
+    Usuario::Usuario(std::string _nickname, std::string _nombre, std::string _contrasena, std::string _email){
+        nickname = _nickname;
+        nombre = _nombre;
+        contrasena = _contrasena;
+        email = _email;
+        
+    }
+
+    bool Usuario::operator<(Usuario& other){
+        return getNickname() < other.getNickname();
+    }
+
+    bool Usuario::esIgual(Usuario& other){
+        return getNickname() = other.getNickname();
+    }
