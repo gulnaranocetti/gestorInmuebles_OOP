f2333a3303a0393f5dad87450a70bbaf22c99876
Anthony David Figueredo Pereira <anthony.figueredo@fing.edu.uy>
2025-06-11 18:42:37 -0300

    Cliente.h

--- include/Cliente.h
+++ include/Cliente.h
@@ -1,16 +1,30 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 #include "Usuario.h"
+#include "Notificacion.h"
+#include "Inmobiliaria.h"
 #include <string>
+#include <set>
 
 class Cliente : public Usuario {
     private:
         std::string apellido;
         std::string documento;
-
+        std::set<Notificacion*> notificaciones;
+        std::set<Inmobiliaria*> suscripciones;
     public:
-        Cliente(std::string nickname, std::string contrasena, std::string nombre, std::string email, std::string apellido, std::string documento);
+        void setApellido(std::string _apellido);
+        void setDocumento(std::string _documento);
+
+        std::string getApellido();
+        std::string getDocumento();
+        std::set<Notificacion> consultarNotificaciones();
+
+        void recibirNotificacion(Notificacion* n);
+        void eliminarSuscripcion(inmobiliaria* i);
+
+        Cliente(std::string _nickname, std::string _contrasena, std::string _nombre, std::string _email, std::string _apellido, std::string _documento);
         ~Cliente();
 };
-
+  
 #endif
