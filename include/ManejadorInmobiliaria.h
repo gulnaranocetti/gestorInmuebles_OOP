5f2300ee3e09972a44aa36e1c338e99fa0cec556
Gulnara <gulnara.nocetti@fing.edu.uy>
2025-06-12 16:11:49 -0300

    ControladorSistema.cpp

--- include/ManejadorInmobiliaria.h
+++ include/ManejadorInmobiliaria.h
@@ -7,6 +7,7 @@
 #include "TipoPublicacion.h"
 #include "TipoInmueble.h"
 #include "Inmobiliaria.h"
+#include "string" 
 #include <set>
 
 class ManejadorInmobiliaria{
@@ -22,7 +23,7 @@ class ManejadorInmobiliaria{
 
         std::set<Inmobiliaria> getInmobiliarias();
 
-        Inmobiliaria getInmobiliaria(String nicknameInmobiliaria);
+        Inmobiliaria getInmobiliaria(std::string nicknameInmobiliaria);
 
         ~ManejadorInmobiliaria();
 };
